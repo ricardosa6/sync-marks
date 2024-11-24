80f4f69ea1d4e975bcc63cd23bd7052db8b22458
Ricardo Sánchez Arias <r.sanchezarias6@gmail.com>
2024-11-24 19:57:42 +0100

    config tests

---
+++ src/modules/auth/components/LogoutButton/LogoutButton.test.tsx
@@ -0,0 +1,59 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { vi, describe, afterEach, beforeEach, it, Mock, expect } from "vitest";
+import { useAuthContext } from "@/modules/auth/contexts";
+import { LogoutButton } from "./LogoutButton";
+
+vi.mock("react-i18next", () => ({
+  useTranslation: () => ({
+    t: (key: string) => key,
+  }),
+}));
+
+vi.mock("@/modules/auth/contexts", () => ({
+  useAuthContext: vi.fn(),
+}));
+
+describe("<LogoutButton />", () => {
+  const mockLogout = vi.fn().mockResolvedValue(null);
+
+  beforeEach(() => {
+    (useAuthContext as Mock).mockReturnValue({
+      logout: mockLogout,
+    });
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("renders the logout button", () => {
+    render(<LogoutButton />);
+    expect(screen.getByText("signOut.buttonLabel")).toBeInTheDocument();
+  });
+
+  it("opens the modal on button click", () => {
+    render(<LogoutButton />);
+    fireEvent.click(screen.getByText("signOut.buttonLabel"));
+    expect(screen.getByText("signOut.modal.title")).toBeInTheDocument();
+    expect(screen.getByText("signOut.modal.message")).toBeInTheDocument();
+  });
+
+  it("calls logout and closes the modal on confirm", async () => {
+    render(<LogoutButton />);
+    fireEvent.click(screen.getByText("signOut.buttonLabel"));
+
+    fireEvent.click(screen.getAllByText("signOut.buttonLabel")[1]);
+
+    expect(mockLogout).toHaveBeenCalled();
+  });
+
+  it("closes the modal on close button click", () => {
+    render(<LogoutButton />);
+
+    fireEvent.click(screen.getByText("signOut.buttonLabel"));
+
+    fireEvent.click(screen.getByText("common.cancel"));
+
+    expect(screen.queryByText("signOut.modal.title")).not.toBeInTheDocument();
+  });
+});
--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,10 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
+    // css: true
+  },
 });
