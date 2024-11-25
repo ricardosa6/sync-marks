96f92ec23bbd92b6fffd2fb1e3bba93f740664f4
Ricardo Sánchez Arias <r.sanchezarias6@gmail.com>
2024-11-25 20:15:04 +0100

    Revert "fix warnings"
    This reverts commit c35bede7c126e08d8fb35c83fdea9670110c23fa.

--- src/modules/auth/components/LogoutButton/LogoutButton.test.tsx
+++ src/modules/auth/components/LogoutButton/LogoutButton.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, act } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { vi, describe, afterEach, beforeEach, it, Mock, expect } from "vitest";
 import { useAuthContext } from "@/modules/auth/contexts";
 import { LogoutButton } from "./LogoutButton";
@@ -33,30 +33,26 @@ describe("<LogoutButton />", () => {
 
   it("opens the modal on button click", () => {
     render(<LogoutButton />);
-    act(() => {
-      fireEvent.click(screen.getByText("signOut.buttonLabel"));
-    });
-
+    fireEvent.click(screen.getByText("signOut.buttonLabel"));
     expect(screen.getByText("signOut.modal.title")).toBeInTheDocument();
     expect(screen.getByText("signOut.modal.message")).toBeInTheDocument();
   });
 
   it("calls logout and closes the modal on confirm", async () => {
     render(<LogoutButton />);
-    act(() => {
-      fireEvent.click(screen.getByText("signOut.buttonLabel"));
-      fireEvent.click(screen.getAllByText("signOut.buttonLabel")[1]);
-    });
+    fireEvent.click(screen.getByText("signOut.buttonLabel"));
+
+    fireEvent.click(screen.getAllByText("signOut.buttonLabel")[1]);
 
     expect(mockLogout).toHaveBeenCalled();
   });
 
   it("closes the modal on close button click", () => {
     render(<LogoutButton />);
-    act(() => {
-      fireEvent.click(screen.getByText("signOut.buttonLabel"));
-      fireEvent.click(screen.getByText("common.cancel"));
-    });
+
+    fireEvent.click(screen.getByText("signOut.buttonLabel"));
+
+    fireEvent.click(screen.getByText("common.cancel"));
 
     expect(screen.queryByText("signOut.modal.title")).not.toBeInTheDocument();
   });
